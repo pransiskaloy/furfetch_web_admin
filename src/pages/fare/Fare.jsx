ff93c765087579f3fb98eaff40b0069022fcf5f4
Francis Bagas <bagas.franciscarlo.t@gmail.com>
2023-03-06 11:52:33 +0800

    no change

--- src/pages/fare/Fare.jsx
+++ src/pages/fare/Fare.jsx
@@ -2,11 +2,28 @@ import "./fare.scss";
 
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import React from 'react'
+import { useState,useEffect,React }  from 'react'
+import {ref, onValue} from 'firebase/database'
+import {db,} from "../../services/firebase.js"
 import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export default function Fare() {
+  const [data, setData] = useState([]);
+  const fareRef = ref(db,'BaseFare');
 
+  useEffect(() => {
+    baseFare(fareRef);
+  },[])
+
+  const baseFare = (fareRefProp) =>  {
+    onValue(fareRefProp,(snapshot) =>{
+      const dataCheck = snapshot.val();
+      setData(dataCheck);
+      console.log(dataCheck)
+    })
+  }
     
   return (
       <div className="fare">
@@ -14,7 +31,42 @@ export default function Fare() {
         <div className="fareContainer">
           <Navbar />
           <div className="fareTitle">
-            Fare Setting
+            <h3>List of Fare</h3>
+          </div>
+          <div className="fareBody">
+            <div>
+              <TextField
+              required
+              label="Booking Fee"
+              defaultValue={data.BookingFee}
+              id="outlined-read-only-input"
+              InputProps={{
+                readOnly: true,
+              }}
+              />
+            </div>
+            <div>
+              <TextField
+              required
+              label="Per Km"
+              defaultValue={data.PerKm}
+              id="outlined-read-only-input"
+              InputProps={{
+                readOnly: true,
+              }}
+              />
+            </div>
+            <div>
+              <TextField
+              required
+              label="Per Minute"
+              defaultValue={data.PerMin}
+              id="outlined-read-only-input"
+              InputProps={{
+                readOnly: true,
+              }}
+              />
+            </div>
           </div>
         </div>
       </div>
